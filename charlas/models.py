b7e454db913b89596162f106dc7157a86646235e
RobertoEsteban <roberto.uach@gmail.com>
2013-08-20 00:22:37 -0400

    Creacion del la clase Speaker y configuracion de la BD

--- charlas/models.py
+++ charlas/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+import datetime
+from thumbs import ImageWithThumbsField
+import os
+from django import forms
 
 # Create your models here.
+
+#path project
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+"""
+List option to select the sex
+"""
+GENDER_CHOICES = (
+    ('M',('Masculino')),
+    ('F',('Femenimo')),
+)
+
+
+class Speaker(models.Model):
+	first_name = models.CharField(_('Nombre'), max_length=100)
+	last_name = models.CharField(_('Nombre'), max_length=100)
+	birthday = models.DateField(_('Fecha de Nacimiento'), help_text= 'dd/mm/yyyy')
+	gender = models.CharField(_('Genero'),max_length=1,choices=GENDER_CHOICES,blank=True, null=True)
+	email = models.EmailField(_('Email'),max_length=100,blank=True, null=True)
+	avatar =  ImageWithThumbsField(_('Avatar'),upload_to="person/", blank=True, null=True, sizes=((50,50),(200,200)))
+	contact = models.CharField(_('Contacto Twitter'), max_length=100,blank=True, null=True)	
+	date = models.DateTimeField(_('Fecha'), default=datetime.datetime.today(),editable=False)
+
+
+	def __unicode__(self):
+		return "%s" %(self.full_name())
+
+	def full_name(self):
+		return self.first_name + ' ' + self.last_name
+
+	full_name.short_description = _('Nombre Completo')
+
+    	class Meta:
+        	verbose_name = _('Nombre Completo')
+        	verbose_name_plural = _('Nombre Completo')
+
--- continuumtour/settings.py
+++ continuumtour/settings.py
@@ -4,19 +4,19 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+     ('RobertoEsteban', 'roberto.uach@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'continuumtour',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'continuumtour',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'charlas',
 )
 
 # A sample logging configuration. The only tangible logging
